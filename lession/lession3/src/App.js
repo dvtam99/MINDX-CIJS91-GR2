f714ec76d34ed45cd4f0782e8c0a392af09b2db5
tamdv <tamdv@jitsinovationlabs.com>
2023-12-22 22:04:57 +0700

    lession 5

--- lession/lession3/src/App.js
+++ lession/lession3/src/App.js
@@ -1,17 +1,37 @@
 // import logo from "./logo.svg";
+
 import "./App.css";
 import Profile from "./component/Profile";
-
+import Counter from "./component/Counter";
+import Person from "./component/Person";
+import { useState } from "react";
 function App() {
+  // let count = 0;
   let imgUrl =
     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Katherine_Johnson_1983.jpg/500px-Katherine_Johnson_1983.jpg";
   const onCLickBtn = () => {
-    console.log("clicked me");
+    // count = count + 1;
+    setCount(count + 1);
+
+    console.log("clicked me 12, " + count);
+    document.getElementById("btn").innerText = count;
+    return null;
   };
+
+  const [count, setCount] = useState(0);
+
+  // let count = 0;
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={onCLickBtn}>Click</button>
+        <Counter />
+        <Counter />
+        <Counter />
+        <Person />
+        <button onClick={onCLickBtn} id="btn">
+          Click {count}
+        </button>
         <Profile imgUrl={imgUrl} name={"Katherine Johnson"} />
       </header>
     </div>
